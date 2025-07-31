cf971755e2fb60e59f7c12b31bc3f545e5fa4298
MD ASIF IQBAL <120092229+AsifIqbal011@users.noreply.github.com>
2025-08-01 01:08:19 +0600

    Database configuration in settings.py

--- backend/shopmate/settings.py
+++ backend/shopmate/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shopmate',
         'USER': 'postgres',
-        'PASSWORD': 'asifDB01',
+        'PASSWORD': '*********', #provide password
         'HOST': 'localhost',
         'PORT': '5432',
     }
